b5b550446c142c363bdcef729588bf5616fd488f
buyath <buyaththayub@gmail.com>
2026-06-02 22:30:07 +0000

    server routing revition done

--- nodejs-revition/core-modules/http.js
+++ nodejs-revition/core-modules/http.js
@@ -1,10 +1,28 @@
 const http  = require('http');
 
 const server = http.createServer((req,res) => {
-    res.writeHead(200,{'content-type' : 'text/plain'});
-    // res.end(`{"name" : "logesh"}`); //  here  we send  json data;
-    res.end("Hello From Node js Server");
+    // res.writeHead(200,{'content-type' : 'text/plain'});
+    // // res.end(`{"name" : "logesh"}`); //  here  we send  json data;
+    // res.end("Hello From Node js Server")
+    if(req.url == "/"){
+        res.writeHead(200,{'content-type' : 'text/plain'});
+        res.end("Home Page")
+    } 
+    else if (req.url == "/about"){
+        res.writeHead(200,{'content-type' : 'text/plain'});
+        res.end("About Page")
+    }
+    else if(req.url == "/contact") {
+        res.writeHead(200,{'content-type' : 'text/plain'});
+        res.end("Contact Page")
+    }
+    else{
+        res.writeHead(404,{'content-type' : 'text/plain'});
+        res.end("Page Not Found");
+    }
 });
+
+// this is how we do server routing with every request 
 
 server.listen(5000,() => { 
     console.log("Server listening http://localhost:5000");
