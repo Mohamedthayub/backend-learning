447186749a9e6426338a4ebb79177e950341fbd2
buyath <buyaththayub@gmail.com>
2026-07-21 17:19:50 +0000

    today docker learning tasks added

---
+++ sql-revition/level-5/level-5.sql
@@ -0,0 +1,19 @@
+/*
+Level 5: Joins (41–50)
+
+Tables
+
+Employees
+Departments
+Projects
+Show employee name with department name.
+Show employees without department.
+Show all departments even without employees.
+Count employees in each department.
+Show employees working on projects.
+Show projects without employees.
+Show employee, department and project together.
+Find department having highest average salary.
+Find employees earning more than department average.
+Find manager of every employee.
+*/
