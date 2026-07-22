174443cc7cf49800becbdbb04104ebfade5c4098
buyath <buyaththayub@gmail.com>
2026-07-22 17:48:33 +0000

    learnt how to save data using mongoose

--- mongoose-learning/models/userModel.js
+++ mongoose-learning/models/userModel.js
@@ -1,19 +1,9 @@
 const mongoose = require('mongoose');
 
-const userSchema = mongoose.Schema({
+const userSchema = new mongoose.Schema({
     name:String,
-    age:Number,        
-    email:String,
-    createdAt:Date,
-    updateAt:Date,
-    bestFriend:mongoose.SchemaTypes.ObjectId,
-    hobbies:[String],
-    address:{
-        city:String,
-        street:String
-    }
-    
-})
+    age: Number
+});
 
-const userModel = mongoose.model('User', userSchema);
-module.exports = userModel;
+const userModel = mongoose.model("user",userSchema);
+module.exports  = userModel;
--- mongoose-learning/script.js
+++ mongoose-learning/script.js
@@ -1,23 +1,29 @@
-const  mongoose = require('mongoose');
-const  User = require("./models/userModel");
+const User = require("./models/userModel");
+const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://127.0.0.1:27017/jvlcode')
-.then(() => {
-    console.log("connected");
+mongoose.connect('mongodb://127.0.0.1:27017/jvlcode')    
+.then((res)=> {
+    console.log("Connected")
 })
-.catch((err) => {
-    console.log('connection error');
-})
+.catch((error) => {
+    console.log("Error",error);
+});
 
-async function run(){
-    const newUser = await User.create({
-        name:'Logesh',
-        age:20,
-        hobbies:['sports','Music'],
-        address:{
-            street:'South Thaikka Street'
-        }
-    })
-    console.log(newUser);
+// const ob
+async  function createUser() {
+    try{
+        // console.log(err);
+        const  user = await User.create({
+            name:"Logesh",
+            age:20
+        })
+        user.name = "Venkat"
+        await user.save();
+        console.log(user);
+    }
+    catch(error){
+        console.log("Error Crating Student",error);
+    }
 }
-run();
+
+createUser();
