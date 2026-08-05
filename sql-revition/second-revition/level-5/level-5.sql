6d2770cdfbcdc89f3e592585e32987539d9627dc
buyath <buyaththayub@gmail.com>
2026-08-05 14:12:57 +0000

    completed mongodb second revition level 2

--- sql-revition/second-revition/level-5/level-5.sql
+++ sql-revition/second-revition/level-5/level-5.sql
@@ -1,7 +1,11 @@
 /*
 Level 5 - HAVING (10 Exercises)
 Cities having more than 5 customers.
+select city,count(*) from customers group by city having count(*) > 5;
+---------------------------------------------------------------------------
 Categories with more than 10 products.
+select category,count(*) from products group by category having count(*) > 10
+-------------------------------------------------------
 Customers who placed more than 3 orders.
 Products ordered more than 20 times.
 Categories whose average price exceeds 1000.
@@ -11,3 +15,8 @@ Categories having total stock greater than 100.
 Products sold more than 50 units.
 Customers with total purchases greater than 5000 (requires JOIN).
 */
+
+use PRACTICE;
+select category,count(*) from products group by category having count(*) > 10
+
+select * from orders;
