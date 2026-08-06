95a4d6cd93ae73b8dbcaab7d132aaa66b6a4aa09
buyath <buyaththayub@gmail.com>
2026-08-06 08:11:28 +0000

    mongodb second revition completed till level 5

--- sql-revition/second-revition/level-5/level-5.sql
+++ sql-revition/second-revition/level-5/level-5.sql
@@ -7,10 +7,18 @@ Categories with more than 10 products.
 select category,count(*) from products group by category having count(*) > 10
 -------------------------------------------------------
 Customers who placed more than 3 orders.
+select customer_id ,count(*) from orders group by customer_id  having count(*) > 3;
+--------------------------------------------------------------
 Products ordered more than 20 times.
+------------------------------------------------------------------
 Categories whose average price exceeds 1000.
+select category,avg(price) from products  group by category having avg(price) > 1000;
+---------------------------------------------------------------------
 Customers purchasing more than 10 items.
+--------------------------------------------------
 Cities whose average customer age exceeds 30.
+select city, avg(age) from customers  group by city having avg(age) > 30;
+-----------------------------------------------------
 Categories having total stock greater than 100.
 Products sold more than 50 units.
 Customers with total purchases greater than 5000 (requires JOIN).
@@ -19,4 +27,8 @@ Customers with total purchases greater than 5000 (requires JOIN).
 use PRACTICE;
 select category,count(*) from products group by category having count(*) > 10
 
-select * from orders;
+select customer_id ,count(*) from orders group by customer_id  having count(*) > 3;
+
+select category,avg(price) from products  group by category having avg(price) > 1000;
+
+select city, avg(age) from customers  group by city having avg(age) > 30;
