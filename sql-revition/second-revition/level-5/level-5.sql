8665a30e3d3311906f8786e6ba63c48f3ac7bcd8
buyath <buyaththayub@gmail.com>
2026-08-07 07:29:23 +0000

    completed level 5 exercise in second revition

--- sql-revition/second-revition/level-5/level-5.sql
+++ sql-revition/second-revition/level-5/level-5.sql
@@ -10,25 +10,25 @@ Customers who placed more than 3 orders.
 select customer_id ,count(*) from orders group by customer_id  having count(*) > 3;
 --------------------------------------------------------------
 Products ordered more than 20 times.
+select count(*) from orders group by  product_id having count(*) > 20;
 ------------------------------------------------------------------
 Categories whose average price exceeds 1000.
 select category,avg(price) from products  group by category having avg(price) > 1000;
 ---------------------------------------------------------------------
 Customers purchasing more than 10 items.
+select sum(quantity) from orders group by  quantity having sum(quantity) > 10;
 --------------------------------------------------
 Cities whose average customer age exceeds 30.
 select city, avg(age) from customers  group by city having avg(age) > 30;
 -----------------------------------------------------
 Categories having total stock greater than 100.
+select category,sum(stock) from  products GROUP BY category HAVING sum(stock) > 100;
+--------------------------------------------------------
 Products sold more than 50 units.
+select sum(quantity) from orders group by product_id  having sum(quantity) > 50;
+----------------------------------------------------------------------------
 Customers with total purchases greater than 5000 (requires JOIN).
 */
 
 use PRACTICE;
-select category,count(*) from products group by category having count(*) > 10
 
-select customer_id ,count(*) from orders group by customer_id  having count(*) > 3;
-
-select category,avg(price) from products  group by category having avg(price) > 1000;
-
-select city, avg(age) from customers  group by city having avg(age) > 30;
